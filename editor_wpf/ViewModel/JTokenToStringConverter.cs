bac52eca0274597592e72f8d185602ec84ccab1a
dm84 <dmitriy.o84@ya.ru>
2015-01-21 16:35:50 +0500

    inception

---
+++ editor_wpf/ViewModel/JTokenToStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+using Newtonsoft.Json.Linq;
+
+namespace editor_wpf.ViewModel
+{
+	class JTokenToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType,
+			object parameter, CultureInfo culture)
+		{
+			JToken token = value as JToken;
+
+			parameter = "Ok";
+
+			return token;
+		}
+
+		public object ConvertBack(object value, Type targetType,
+			object parameter, CultureInfo culture)
+		{
+
+
+			return new JValue(value);
+		}
+
+	}
+}
